defa15302d3de614a4b7bed41302c3ba35d6027a
Tun Nanda Aung <tunnandaaung@outlook.com>
2022-11-03 18:58:38 +0900

    Add example

---
+++ example/android/app/src/main/kotlin/com/example/heroicons_flutter_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.heroicons_flutter_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/heroicons_flutter.dart
+++ lib/heroicons_flutter.dart
@@ -4,14 +4,14 @@ import 'package:flutter/material.dart';
 
 /// A Calculator.
 class HeroiconsFlutter {
-  static const IconData academicCap = HeroiconsIconData(0xe804);
+  static const IconData academicCap = _HeroiconsIconData(0xe804);
 }
 
-class HeroiconsIconData extends IconData {
-  const HeroiconsIconData(int code)
+class _HeroiconsIconData extends IconData {
+  const _HeroiconsIconData(int code)
       : super(
           code,
-          fontFamily: 'Material Design Icons',
-          fontPackage: 'material_design_icons_flutter',
+          fontFamily: 'Heroicons Flutter',
+          fontPackage: 'heroicons_flutter',
         );
 }
